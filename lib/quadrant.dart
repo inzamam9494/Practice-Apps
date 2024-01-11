0ee5b1264c70e5101b8c078798ede60bd52a176b
mohdi <mohdinzamam12125@gmail.com>
2024-01-11 11:54:22 +0530

    Quadrant App

--- lib/quadrant.dart
+++ lib/quadrant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:happy_birthday/businessCard.dart';
 
 class Quadrant extends StatelessWidget {
   const Quadrant({super.key});
@@ -7,27 +8,120 @@ class Quadrant extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 20,
-                decoration: const BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    color: Colors.purpleAccent),
-                child: const Text("Text Widgets"),
+              Row(
+                children: [
+                  Container(
+                    height: 400,
+                    width: 200,
+                    color: Colors.cyanAccent,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Text Widgets",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),),
+                        SizedBox(height: 20),
+                        Padding(
+                          padding: EdgeInsets.all(20),
+                            child: Text("Displays text and follows the recommended Material Design guidelines."))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 400,
+                    width: 200,
+                    color: Colors.purpleAccent,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Image Widgets",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),),
+                        SizedBox(height: 20),
+                        Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text("Creates a composable that lays out and draws a given Painter class object."))
+                      ],
+                    ),
+                  )
+                ],
               )
             ],
           ),
           Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                child: const Text("Image Widgets"),
-                color: Colors.amber,
+              Row(
+                children: [
+                  Container(
+                    height: 400,
+                    width: 200,
+                    color: Colors.greenAccent,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Row Widgets",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),),
+                        SizedBox(height: 20),
+                        Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text("A layout composable that places its children in a horizontal sequence."))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    height: 400,
+                    width: 200,
+                    color: Colors.amberAccent,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Column Widgets",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold
+                          ),),
+                        SizedBox(height: 20),
+                        Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text("A layout composable that places its children in a vertical sequence."))
+                      ],
+                    ),
+                  )
+                ],
               )
             ],
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: 350,
+            child: ElevatedButton(onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (BuildContext context) => const BusinessCard()));
+            },
+                style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)
+                ),
+                child: const Text("NEXT",
+                style: TextStyle(color: Colors.white),)),
           )
         ],
       ),
